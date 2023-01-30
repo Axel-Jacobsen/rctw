19ed354d74c41bdff4e59d16c857e6e58de68608
Axel Jacobsen <axelnj44@gmail.com>
2023-01-29 21:52:01 -0800

    rans stream work

--- prototypes/streaming.py
+++ prototypes/streaming.py
@@ -49,9 +49,14 @@ def stream_encode(
     output_stream: List[int] = []
 
     for symbol in input_seq:
-        while state not in Is[symbol]:
-            output_stream.append(state % b)
+        while state >= b * freqs[symbol]:
+            next_out_bit = state % b
+            output_stream.append(next_out_bit)
             state //= b
+
+            if state == 0:
+                raise Exception
+
         state = coder.C(symbol, state)
 
     return output_stream, state
@@ -75,22 +80,17 @@ def stream_decode(
     decoded_stream: List[int] = []
     state = init_state
 
-    while state != final_state:
+    for _ in range(M + 1):
         symbol, state = coder.D(state)
         decoded_stream.append(symbol)
 
-        while state not in I:
+        while state < I[0]:
             state = state * b + coded_seq.pop()
 
     return decoded_stream
 
 
-if __name__ == "__main__":
-    import random
-
-    freqs = {9: 3, 1: 3, 2: 2}
-    coder = rANS(freqs)
-
+def coder_decoder_test(coder, freqs):
     for k in freqs:
         for i in range(0, sum(freqs.values())):
             # C \circ D = D \circ C = id
@@ -98,13 +98,25 @@ if __name__ == "__main__":
             woopsies_string = f"{coder.D(coder.C(k, i))=} == {(k, i)=}"
             assert cond, woopsies_string
 
+
+if __name__ == "__main__":
+    import random
+
+    freqs = {0: 3, 1: 3, 2: 2}
+    coder = rANS(freqs)
+
+    coder_decoder_test(coder, freqs)
+
     vs = freqs.items()
     input_seq = random.choices([v[0] for v in vs], weights=[v[1] for v in vs], k=10)
-    print(input_seq)
+    input_seq = [0, 1, 0, 2, 2, 0, 2, 1, 2]
 
-    output_stream, fin_state = stream_encode(coder, input_seq, freqs)
-    decoded = stream_decode(coder, output_stream, fin_state, freqs)
+    l = 1
+    b = 2
+    output_stream, fin_state = stream_encode(coder, input_seq, freqs, l=l, b=b)
 
-    assert (
-        input_seq == decoded[::-1]
-    ), f"\n{input_seq=}\n{  decoded[::-1]=}\n{input_seq=}"
+    print(f"encoded_stream={output_stream}, {fin_state=}")
+
+    decoded = stream_decode(coder, output_stream, fin_state, freqs, l=l, b=b)
+
+    assert input_seq == decoded[::-1], f"\n{input_seq=}\n{  decoded[::-1]=}"
